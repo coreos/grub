9f4689e21d9305585cc607c491e880432cf5453c
okuji <okuji@localhost>
2002-02-07 23:28:47 +0000

    2002-02-08  Yoshinori K. Okuji  <okuji@enbug.org>
    	* stage2/fsys_jfs.c (jfs_read) [STAGE1_5]: Set and reset
    	DISK_READ_FUNC even in Stage 1.5.
    	* stage2/fsys_xfs.c (xfs_read) [STAGE1_5]: Likewise.
    
    	* stage2/stage1_5.c (saved_sector): Initialized with -1.
    	(cmain): Check if SAVED_SECTOR was set appropriately after
    	reading the second sector of Stage 2. If SAVED_SECTOR is not
    	set (i.e. it is equal to -1), print an error and stop.

--- stage2/fsys_xfs.c
+++ stage2/fsys_xfs.c
@@ -1,7 +1,7 @@
 /* fsys_xfs.c - an implementation for the SGI XFS file system */
 /*  
  *  GRUB  --  GRand Unified Bootloader
- *  Copyright (C) 2001  Free Software Foundation, Inc.
+ *  Copyright (C) 2001,2002  Free Software Foundation, Inc.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -499,14 +499,12 @@ xfs_read (char *buf, int len)
 			endofcur = (offset + xadlen) << xfs.blklog; 
 			toread = (endofcur >= endpos)
 				  ? len : (endofcur - filepos);
-#ifndef STAGE1_5
+
 			disk_read_func = disk_read_hook;
-#endif /* STAGE1_5 */
 			devread (fsb2daddr (xad->start),
 				 filepos - (offset << xfs.blklog), toread, buf);
-#ifndef STAGE1_5
 			disk_read_func = NULL;
-#endif /* STAGE1_5 */
+
 			buf += toread;
 			len -= toread;
 			filepos += toread;
