5aa541e6ebdadfa605ba0e248640eab9e7b5f007
robertmh <robertmh@localhost>
2008-01-08 11:38:18 +0000

    2008-01-08  Robert Millan  <rmh@aybabtu.com>
            * loader/i386/pc/linux.c (grub_rescue_cmd_linux): For
            `! grub_linux_is_bzimage', change order of address comparison to make
            it more intuitive, and improve "too big zImage" error message.

--- loader/i386/pc/linux.c
+++ loader/i386/pc/linux.c
@@ -146,10 +146,12 @@ grub_rescue_cmd_linux (int argc, char *argv[])
   grub_linux_tmp_addr = (char *) GRUB_LINUX_BZIMAGE_ADDR + prot_size;
 
   if (! grub_linux_is_bzimage
-      && prot_size > (grub_size_t) (grub_linux_real_addr
-				    - (char *) GRUB_LINUX_ZIMAGE_ADDR))
+      && ((char *) GRUB_LINUX_ZIMAGE_ADDR + prot_size
+	  > (grub_size_t) grub_linux_real_addr))
     {
-      grub_error (GRUB_ERR_BAD_OS, "too big zImage, use bzImage instead");
+      grub_error (GRUB_ERR_BAD_OS, "too big zImage (0x%x > 0x%x), use bzImage instead",
+		  (char *) GRUB_LINUX_ZIMAGE_ADDR + prot_size,
+		  (grub_size_t) grub_linux_real_addr);
       goto fail;
     }
   
