da9a6a946349f352efe6edf2fae7e5209ab72204
robertmh <robertmh@localhost>
2008-05-29 13:01:43 +0000

    2008-05-29  Robert Millan  <rmh@aybabtu.com>
            * fs/affs.c: Update copyright year.
            * fs/ext2.c: Likewise.
            * fs/fshelp.c: Likewise.
            * fs/hfsplus.c: Likewise.
            * fs/ntfs.c: Likewise.
            * fs/xfs.c: Likewise.
            * include/grub/fshelp.h: Likewise.
            * util/grub-mkdevicemap.c: Likewise.

--- util/grub-mkdevicemap.c
+++ util/grub-mkdevicemap.c
@@ -1,7 +1,7 @@
 /* grub-mkdevicemap.c - make a device map file automatically */
 /*
  *  GRUB  --  GRand Unified Bootloader
- *  Copyright (C) 1999,2000,2001,2002,2003,2004,2005,2007 Free Software Foundation, Inc.
+ *  Copyright (C) 1999,2000,2001,2002,2003,2004,2005,2007,2008 Free Software Foundation, Inc.
  *
  *  GRUB is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
