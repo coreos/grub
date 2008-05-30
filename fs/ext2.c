811d3878d8a168294dacec94dcd584886eeca22b
robertmh <robertmh@localhost>
2008-05-30 20:47:43 +0000

    2008-05-30  Robert Millan  <rmh@aybabtu.com>
            * fs/ext2.c (grub_ext2_uuid): Use `04x' instead of '02x' as
            grub_printf() flags, since we're printing in units of 2 bytes.

--- fs/ext2.c
+++ fs/ext2.c
@@ -886,7 +886,7 @@ grub_ext2_uuid (grub_device_t device, char **uuid)
   if (data)
     {
       *uuid = grub_malloc (40 + sizeof ('\0'));
-      grub_sprintf (*uuid, "%02x%02x-%02x-%02x-%02x-%02x%02x%02x",
+      grub_sprintf (*uuid, "%04x%04x-%04x-%04x-%04x-%04x%04x%04x",
 		    grub_be_to_cpu16 (data->sblock.uuid[0]), grub_be_to_cpu16 (data->sblock.uuid[1]),
 		    grub_be_to_cpu16 (data->sblock.uuid[2]), grub_be_to_cpu16 (data->sblock.uuid[3]),
 		    grub_be_to_cpu16 (data->sblock.uuid[4]), grub_be_to_cpu16 (data->sblock.uuid[5]),
