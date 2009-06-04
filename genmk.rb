cf00df31670f11a16773347713e595a204b99b2b
phcoder <phcoder@localhost>
2009-06-04 20:54:14 +0000

    2009-06-04  Vladimir Serbinenko  <phcoder@gmail.com>
    	Use objconv when compiling with Apple's CC
    
    	* conf/i386-pc.rmk (efiemu32.o): use OBJCONV if defined
    	(efiemu64.o): likewise
    	(efiemu64_c.o): omit -mcmodel=large and add -DAPPLE_CC=1
    	when compiling with Apple's CC
    	(efiemu64_s.o): likewise
    	* configure.ac: check for objconv when compiling with Apple's CC
    	* genmk.rb: use objconv for modules when compiled with Apple's CC

--- genmk.rb
+++ genmk.rb
@@ -112,11 +112,20 @@ endif
 MOSTLYCLEANFILES += #{deps_str}
 UNDSYMFILES += #{undsym}
 
+ifneq ($(TARGET_APPLE_CC),1)
 #{@name}: #{pre_obj} #{mod_obj} $(TARGET_OBJ2ELF)
 	-rm -f $@
 	$(TARGET_CC) $(#{prefix}_LDFLAGS) $(TARGET_LDFLAGS) -Wl,-r,-d -o $@ #{pre_obj} #{mod_obj}
 	if test ! -z $(TARGET_OBJ2ELF); then ./$(TARGET_OBJ2ELF) $@ || (rm -f $@; exit 1); fi
 	$(STRIP) --strip-unneeded -K grub_mod_init -K grub_mod_fini -K _grub_mod_init -K _grub_mod_fini -R .note -R .comment $@
+else
+#{@name}: #{pre_obj} #{mod_obj} $(TARGET_OBJ2ELF)
+	-rm -f $@
+	-rm -f $@.bin
+	$(TARGET_CC) $(#{prefix}_LDFLAGS) $(TARGET_LDFLAGS) -Wl,-r,-d -o $@.bin #{pre_obj} #{mod_obj}
+	$(OBJCONV) -f$(TARGET_MODULE_FORMAT) -nr:_grub_mod_init:grub_mod_init -nr:_grub_mod_fini:grub_mod_fini -wd1106 -nu -nd $@.bin $@
+	-rm -f $@.bin
+endif
 
 #{pre_obj}: $(#{prefix}_DEPENDENCIES) #{objs_str}
 	-rm -f $@
