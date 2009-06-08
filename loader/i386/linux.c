9e172e3066599484eb7d8e0b13f25ef4f3388c51
robertmh <robertmh@localhost>
2009-06-08 16:12:58 +0000

    2009-06-08  Robert Millan  <rmh.grub@aybabtu.com>
            * loader/i386/linux.c (grub_cmd_linux): When processing `vga=', use
            as fallback an equivalent option without depth.

--- loader/i386/linux.c
+++ loader/i386/linux.c
@@ -761,17 +761,19 @@ grub_cmd_linux (grub_command_t cmd __attribute__ ((unused)),
 		break;
 	      }
 
-	    buf = grub_malloc (20);
+	    buf = grub_malloc (sizeof ("WWWWxHHHHxDD;WWWWxHHHH"));
 	    if (! buf)
 	      goto fail;
 	    
 	    linux_mode 
 	      = &linux_vesafb_modes[vid_mode - GRUB_LINUX_VID_MODE_VESA_START];
 	    
-	    grub_sprintf (buf, "%dx%dx%d", 
+	    grub_sprintf (buf, "%ux%ux%u;%ux%u", 
 			  linux_vesafb_res[linux_mode->res_index].width,
 			  linux_vesafb_res[linux_mode->res_index].height,
-			  linux_mode->depth);
+			  linux_mode->depth,
+			  linux_vesafb_res[linux_mode->res_index].width,
+			  linux_vesafb_res[linux_mode->res_index].height);
 	    grub_printf ("%s is deprecated. "
 			 "Use set gfxpayload=%s before "
 			 "linux command instead.\n", 
