85f2aab6f7e1ad75ffa143a8b5f8589ac8535fb3
proski <proski@localhost>
2009-06-29 03:37:42 +0000

    2009-06-28  Pavel Roskin  <proski@gnu.org>
    	* kern/i386/qemu/mmap.c (grub_machine_mmap_iterate): Use cast to
    	fix a warning.

--- kern/i386/qemu/mmap.c
+++ kern/i386/qemu/mmap.c
@@ -48,7 +48,7 @@ grub_err_t
 grub_machine_mmap_iterate (int NESTED_FUNC_ATTR (*hook) (grub_uint64_t, grub_uint64_t, grub_uint32_t))
 {
   if (hook (0x0,
-	    _start,
+	    (grub_addr_t) _start,
 	    GRUB_MACHINE_MEMORY_AVAILABLE))
     return 1;
 
