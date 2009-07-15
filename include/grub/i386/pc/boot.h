fde24e10a86263cadf274facbe61de1add166078
proski <proski@localhost>
2009-07-15 15:06:12 +0000

    2009-07-15  Pavel Roskin  <proski@gnu.org>
    	* include/grub/i386/pc/boot.h (GRUB_BOOT_MACHINE_BPB_END):
    	Increase to 0x5a to accommodate FAT32.  Adjust other offsets
    	accordingly.
    	Original patch by Yves Blusseau <yves.grub-devel@zetam.org>

--- include/grub/i386/pc/boot.h
+++ include/grub/i386/pc/boot.h
@@ -26,16 +26,16 @@
 #define GRUB_BOOT_MACHINE_BPB_START	0x3
 
 /* The offset of the end of BPB (BIOS Parameter Block).  */
-#define GRUB_BOOT_MACHINE_BPB_END	0x3e
+#define GRUB_BOOT_MACHINE_BPB_END	0x5a
 
 /* The offset of KERNEL_SECTOR.  */
-#define GRUB_BOOT_MACHINE_KERNEL_SECTOR	0x44
+#define GRUB_BOOT_MACHINE_KERNEL_SECTOR	0x5c
 
 /* The offset of BOOT_DRIVE.  */
-#define GRUB_BOOT_MACHINE_BOOT_DRIVE	0x4c
+#define GRUB_BOOT_MACHINE_BOOT_DRIVE	0x64
 
 /* The offset of BOOT_DRIVE_CHECK.  */
-#define GRUB_BOOT_MACHINE_DRIVE_CHECK	0x4e
+#define GRUB_BOOT_MACHINE_DRIVE_CHECK	0x66
 
 /* The offset of a magic number used by Windows NT.  */
 #define GRUB_BOOT_MACHINE_WINDOWS_NT_MAGIC	0x1b8
