1e1ddb6cb924e1b8d260f6e5029766b2b02bf5d2
phcoder <phcoder@debian.bg45.phnet>
2009-10-24 10:56:21 +0200

    init fixes

--- kern/mips/yeeloong/init.c
+++ kern/mips/yeeloong/init.c
@@ -1,3 +1,21 @@
+/*
+ *  GRUB  --  GRand Unified Bootloader
+ *  Copyright (C) 2009  Free Software Foundation, Inc.
+ *
+ *  GRUB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GRUB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <grub/kernel.h>
 #include <grub/misc.h>
 #include <grub/env.h>
@@ -51,26 +69,11 @@ grub_reboot (void)
   while (1);
 }
 
-void
-grub_machine_set_prefix (void)
-{
-  grub_env_set ("prefix", grub_prefix);
-}
-
-extern char _end[];
-
-grub_addr_t
-grub_arch_modules_addr (void)
-{
-  return ALIGN_UP((grub_addr_t) _end + GRUB_MOD_GAP, GRUB_MOD_ALIGN);
-}
-
 grub_err_t 
 grub_machine_mmap_iterate (int NESTED_FUNC_ATTR (*hook) (grub_uint64_t, 
 							 grub_uint64_t, 
 							 grub_uint32_t))
 {
-  hook (0, RAMSIZE,
-	GRUB_MACHINE_MEMORY_AVAILABLE);
+  hook (0, RAMSIZE, GRUB_MACHINE_MEMORY_AVAILABLE);
   return GRUB_ERR_NONE;
 }
