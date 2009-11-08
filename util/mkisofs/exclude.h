4b4c4f64002ab606034c8f7e1d76a74269b92018
Robert Millan <rmh@aybabtu.com>
2009-11-08 22:51:08 +0000

    Import http://www.tux.org/pub/people/eric-youngdale/mkisofs/mkisofs-1.11.3.tar.gz

---
+++ util/mkisofs/exclude.h
@@ -0,0 +1,10 @@
+/*
+ * 9-Dec-93 R.-D. Marzusch, marzusch@odiehh.hanse.de:
+ * added 'exclude' option (-x) to specify pathnames NOT to be included in 
+ * CD image.
+ *
+ * 	$Id: exclude.h,v 1.1 1997/02/23 15:53:19 eric Rel $
+ */
+
+void exclude();
+int is_excluded();
---
+++ util/mkisofs/match.h
@@ -0,0 +1,14 @@
+/*
+ * 27th March 1996. Added by Jan-Piet Mens for matching regular expressions
+ * 		    in paths.
+ * 
+ */
+
+/*
+ * 	$Id: match.h,v 1.1 1997/02/23 15:56:12 eric Rel $
+ */
+
+#include "fnmatch.h"
+
+void add_match();
+int matches();
