a261f807286581cbc86b08fa8141cc617bc6b842
Vladimir 'phcoder' Serbinenko <phcoder@gmail.com>
2009-11-26 17:14:39 +0100

    Added origin notice to lib/pbkdf2.c

--- lib/pbkdf2.c
+++ lib/pbkdf2.c
@@ -16,6 +16,7 @@
    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
 
 /* Written by Simon Josefsson.  */
+/* Imported from gnulib.  */
 
 #include <grub/crypto.h>
 #include <grub/mm.h>
