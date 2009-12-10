febfc12c6ee6b04d5a02320b8741a098738f8057
Robert Millan <rmh@aybabtu.com>
2009-12-10 22:38:54 +0000

    2009-12-10  Robert Millan  <rmh.grub@aybabtu.com>
            * include/grub/mips/libgcc.h: Only export symbols for functions
            that libgcc provides.

--- include/grub/mips/libgcc.h
+++ include/grub/mips/libgcc.h
@@ -1,6 +1,6 @@
 /*
  *  GRUB  --  GRand Unified Bootloader
- *  Copyright (C) 2004,2007  Free Software Foundation, Inc.
+ *  Copyright (C) 2004,2007,2009  Free Software Foundation, Inc.
  *
  *  GRUB is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -16,9 +16,23 @@
  *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <config.h>
+
+#ifdef HAVE___ASHLDI3
 void EXPORT_FUNC (__ashldi3) (void);
+#endif
+#ifdef HAVE___ASHRDI3
 void EXPORT_FUNC (__ashrdi3) (void);
+#endif
+#ifdef HAVE___LSHRDI3
 void EXPORT_FUNC (__lshrdi3) (void);
+#endif
+#ifdef HAVE___UCMPDI2
 void EXPORT_FUNC (__ucmpdi2) (void);
+#endif
+#ifdef HAVE___BSWAPSI2
 void EXPORT_FUNC (__bswapsi2) (void);
+#endif
+#ifdef HAVE___BSWAPDI2
 void EXPORT_FUNC (__bswapdi2) (void);
+#endif
