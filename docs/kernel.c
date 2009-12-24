20bbb84b97277b2418bfd09488a696d2293841b0
Robert Millan <rmh@aybabtu.com>
2009-12-24 14:25:44 +0000

    2009-12-24  Robert Millan  <rmh.grub@aybabtu.com>
    	* docs/multiboot.h: Replace with include/multiboot.h from GRUB 2 Bazaar
    	trunk.
    	* docs/kernel.c (cmain): Update name references for new multiboot.h
    	header.

--- docs/kernel.c
+++ docs/kernel.c
@@ -86,18 +86,18 @@ cmain (unsigned long magic, unsigned long addr)
   /* Are mods_* valid?  */
   if (CHECK_FLAG (mbi->flags, 3))
     {
-      module_t *mod;
+      multiboot_module_t *mod;
       int i;
       
       printf ("mods_count = %d, mods_addr = 0x%x\n",
 	      (int) mbi->mods_count, (int) mbi->mods_addr);
-      for (i = 0, mod = (module_t *) mbi->mods_addr;
+      for (i = 0, mod = (multiboot_module_t *) mbi->mods_addr;
 	   i < mbi->mods_count;
 	   i++, mod++)
-	printf (" mod_start = 0x%x, mod_end = 0x%x, string = %s\n",
+	printf (" mod_start = 0x%x, mod_end = 0x%x, cmdline = %s\n",
 		(unsigned) mod->mod_start,
 		(unsigned) mod->mod_end,
-		(char *) mod->string);
+		(char *) mod->cmdline);
     }
 
   /* Bits 4 and 5 are mutually exclusive!  */
@@ -110,44 +110,44 @@ cmain (unsigned long magic, unsigned long addr)
   /* Is the symbol table of a.out valid?  */
   if (CHECK_FLAG (mbi->flags, 4))
     {
-      aout_symbol_table_t *aout_sym = &(mbi->u.aout_sym);
+      multiboot_aout_symbol_table_t *multiboot_aout_sym = &(mbi->u.aout_sym);
       
-      printf ("aout_symbol_table: tabsize = 0x%0x, "
+      printf ("multiboot_aout_symbol_table: tabsize = 0x%0x, "
 	      "strsize = 0x%x, addr = 0x%x\n",
-	      (unsigned) aout_sym->tabsize,
-	      (unsigned) aout_sym->strsize,
-	      (unsigned) aout_sym->addr);
+	      (unsigned) multiboot_aout_sym->tabsize,
+	      (unsigned) multiboot_aout_sym->strsize,
+	      (unsigned) multiboot_aout_sym->addr);
     }
 
   /* Is the section header table of ELF valid?  */
   if (CHECK_FLAG (mbi->flags, 5))
     {
-      elf_section_header_table_t *elf_sec = &(mbi->u.elf_sec);
+      multiboot_elf_section_header_table_t *multiboot_elf_sec = &(mbi->u.elf_sec);
 
-      printf ("elf_sec: num = %u, size = 0x%x,"
+      printf ("multiboot_elf_sec: num = %u, size = 0x%x,"
 	      " addr = 0x%x, shndx = 0x%x\n",
-	      (unsigned) elf_sec->num, (unsigned) elf_sec->size,
-	      (unsigned) elf_sec->addr, (unsigned) elf_sec->shndx);
+	      (unsigned) multiboot_elf_sec->num, (unsigned) multiboot_elf_sec->size,
+	      (unsigned) multiboot_elf_sec->addr, (unsigned) multiboot_elf_sec->shndx);
     }
 
   /* Are mmap_* valid?  */
   if (CHECK_FLAG (mbi->flags, 6))
     {
-      memory_map_t *mmap;
+      multiboot_memory_map_t *mmap;
       
       printf ("mmap_addr = 0x%x, mmap_length = 0x%x\n",
 	      (unsigned) mbi->mmap_addr, (unsigned) mbi->mmap_length);
-      for (mmap = (memory_map_t *) mbi->mmap_addr;
+      for (mmap = (multiboot_memory_map_t *) mbi->mmap_addr;
 	   (unsigned long) mmap < mbi->mmap_addr + mbi->mmap_length;
-	   mmap = (memory_map_t *) ((unsigned long) mmap
+	   mmap = (multiboot_memory_map_t *) ((unsigned long) mmap
 				    + mmap->size + sizeof (mmap->size)))
 	printf (" size = 0x%x, base_addr = 0x%x%x,"
 		" length = 0x%x%x, type = 0x%x\n",
 		(unsigned) mmap->size,
-		(unsigned) mmap->base_addr_high,
-		(unsigned) mmap->base_addr_low,
-		(unsigned) mmap->length_high,
-		(unsigned) mmap->length_low,
+		mmap->addr >> 32,
+		mmap->addr & 0xffffffff,
+		mmap->len >> 32,
+		mmap->len & 0xffffffff,
 		(unsigned) mmap->type);
     }
 }    
