85312e19336ae0f773b811f1f9f69832d6ef1f75
Robert Millan <rmh@aybabtu.com>
2010-01-18 16:14:58 +0000

    kernel.h adjustments to fix build

--- include/grub/ia64/kernel.h
+++ include/grub/ia64/kernel.h
@@ -1,6 +1,6 @@
 /*
  *  GRUB  --  GRand Unified Bootloader
- *  Copyright (C) 2002,2003,2007,2008,2010  Free Software Foundation, Inc.
+ *  Copyright (C) 2010  Free Software Foundation, Inc.
  *
  *  GRUB is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -16,19 +16,10 @@
  *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef GRUB_MACHINE_KERNEL_HEADER
-#define GRUB_MACHINE_KERNEL_HEADER   1
+#ifndef GRUB_CPU_KERNEL_HEADER
+#define GRUB_CPU_KERNEL_HEADER   1
 
-/* The offset of GRUB_PREFIX.  */
-#define GRUB_KERNEL_MACHINE_PREFIX		0x8
+#define GRUB_MOD_ALIGN	0x1
+#define GRUB_MOD_GAP	0x0
 
-/* End of the data section. */
-#define GRUB_KERNEL_MACHINE_DATA_END		0x50
-
-#ifndef ASM_FILE
-/* The prefix which points to the directory where GRUB modules and its
-   configuration file are located.  */
-extern char grub_prefix[];
-#endif
-
-#endif /* ! GRUB_MACHINE_KERNEL_HEADER */
+#endif /* ! GRUB_CPU_KERNEL_HEADER */
