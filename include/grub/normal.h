e28e32ae51c75a64333deffbd78f1cda9c376fc8
Vladimir 'phcoder' Serbinenko <phcoder@gmail.com>
2010-02-07 16:52:47 +0100

    2010-02-07  Vladimir Serbinenko  <phcoder@gmail.com>
    	* include/grub/normal.h [GRUB_UTIL]: Remove leftover declarations.
    	* include/grub/partition.h [GRUB_UTIL]: Likewise.

--- include/grub/normal.h
+++ include/grub/normal.h
@@ -110,37 +110,4 @@ void read_terminal_list (void);
 
 void grub_set_more (int onoff);
 
-#ifdef GRUB_UTIL
-void grub_normal_init (void);
-void grub_normal_fini (void);
-void grub_hello_init (void);
-void grub_hello_fini (void);
-void grub_ls_init (void);
-void grub_ls_fini (void);
-void grub_cat_init (void);
-void grub_cat_fini (void);
-void grub_boot_init (void);
-void grub_boot_fini (void);
-void grub_cmp_init (void);
-void grub_cmp_fini (void);
-void grub_terminal_init (void);
-void grub_terminal_fini (void);
-void grub_loop_init (void);
-void grub_loop_fini (void);
-void grub_help_init (void);
-void grub_help_fini (void);
-void grub_halt_init (void);
-void grub_halt_fini (void);
-void grub_reboot_init (void);
-void grub_reboot_fini (void);
-void grub_configfile_init (void);
-void grub_configfile_fini (void);
-void grub_search_init (void);
-void grub_search_fini (void);
-void grub_test_init (void);
-void grub_test_fini (void);
-void grub_blocklist_init (void);
-void grub_blocklist_fini (void);
-#endif
-
 #endif /* ! GRUB_NORMAL_HEADER */
