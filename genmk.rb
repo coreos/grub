a8efbf649e9fbacf850cee6c80f8b6aea9b691a5
Vladimir 'phcoder' Serbinenko <phcoder@gmail.com>
2010-03-04 15:41:32 +0100

    2010-03-04  Vladimir Serbinenko  <phcoder@gmail.com>
    	* genmk.rb: Remove terminal*.lst in make clean.

--- genmk.rb
+++ genmk.rb
@@ -201,7 +201,7 @@ endif
 -include #{dep}
 
 clean-module-#{extra_target}.#{@rule_count}:
-	rm -f #{command} #{fs} #{partmap} #{handler} #{parttool} #{video}
+	rm -f #{command} #{fs} #{partmap} #{handler} #{parttool} #{video} #{terminal}
 
 CLEAN_MODULE_TARGETS += clean-module-#{extra_target}.#{@rule_count}
 
