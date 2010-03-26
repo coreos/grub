7c0ab99bd2095fdd081e5c627727a3c878b35409
Vladimir 'phcoder' Serbinenko <phcoder@gmail.com>
2010-03-26 19:23:19 +0100

    Remove  list_iterate usage functional_test.c

--- tests/lib/functional_test.c
+++ tests/lib/functional_test.c
@@ -26,15 +26,10 @@ grub_functional_test (struct grub_extcmd *cmd __attribute__ ((unused)),
 		      int argc __attribute__ ((unused)),
 		      char **args __attribute__ ((unused)))
 {
-  auto int run_test (grub_test_t test);
-  int run_test (grub_test_t test)
-  {
-    grub_test_run (test);
-    return 0;
-  }
+  grub_test_t test;
 
-  grub_list_iterate (GRUB_AS_LIST (grub_test_list),
-		     (grub_list_hook_t) run_test);
+  for (test = grub_test_list; test; test = test->next)
+    grub_test_run (test);
   return GRUB_ERR_NONE;
 }
 
