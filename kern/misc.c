187bbe3d9cf392c7cd7fb413bc001909c1f01068
Grégoire Sutre <gregoire.sutre@gmail.com>
2010-04-03 21:00:21 +0200

    * kern/misc.c: Disable the __enable_execute_stack hack for utilities. 	* include/grub/misc.h: Likewise.

--- kern/misc.c
+++ kern/misc.c
@@ -1058,7 +1058,7 @@ grub_abort (void)
 void abort (void) __attribute__ ((alias ("grub_abort")));
 #endif
 
-#ifdef NEED_ENABLE_EXECUTE_STACK
+#if defined(NEED_ENABLE_EXECUTE_STACK) && !defined(GRUB_UTIL)
 /* Some gcc versions generate a call to this function
    in trampolines for nested functions.  */
 void __enable_execute_stack (void *addr __attribute__ ((unused)))
