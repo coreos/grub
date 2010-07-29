818a356eb196a2c2ca5af7d580b9920c8564ebcc
Paulo de Rezende Pinatti <ppinatti@linux.vnet.ibm.com>
2010-07-29 16:36:17 -0300

    Fixed get_card_packet to correctly read data from network card into buffer.
    * net/ieee1275/interface.c (get_card_packet): read data regardless of ethernet header

--- net/ieee1275/interface.c
+++ net/ieee1275/interface.c
@@ -1,9 +1,4 @@
 #include <grub/net/ieee1275/interface.h>
-#include <grub/time.h>
-#include <grub/mm.h>
-#include <grub/net/ethernet.h>
-#include <grub/net/ip.h>
-#include <grub/net/arp.h>
 #include <grub/net/netbuff.h>
 #include <grub/ieee1275/ofnet.h>
 
@@ -37,56 +32,15 @@ int send_card_buffer (struct grub_net_buff *pack)
 int get_card_packet (struct grub_net_buff *pack __attribute__ ((unused)))
 {
 
-  int actual;
-  char *datap; 
-  struct iphdr *iph;
-  struct etherhdr *eth;
-  struct arphdr *arph;
-  struct ip6hdr *ip6h;
+  int actual, rc;
   pack->data = pack->tail =  pack->head;
-  datap = pack->data; 
   do
   {
-    grub_ieee1275_read (handle,datap,sizeof (*eth),&actual);
+    rc = grub_ieee1275_read (handle,pack->data,1500,&actual);
 
-  }while (actual <= 0);
-  eth = (struct etherhdr *) datap;
-  datap += sizeof(*eth);
- 
+  }while (actual <= 0 || rc < 0);
+  grub_netbuff_put (pack, actual);
 
-  switch (eth->type)
-  {
-    case 0x806:
-
-      grub_ieee1275_read (handle,datap,sizeof (*arph),&actual);
-      arph = (struct arphdr *) datap;
-      
-      grub_netbuff_put (pack,sizeof (*eth) + sizeof (*arph));
-    break;
-    case 0x800:
-      grub_ieee1275_read (handle,datap,sizeof (*iph),&actual);
-      iph = (struct iphdr *) datap;
-      datap += sizeof(*iph);
-
-    
-      grub_ieee1275_read (handle,datap,iph->len - sizeof (*iph),&actual);
-       
-    
-      grub_netbuff_put (pack,sizeof (*eth) + iph->len);
-    break;
-
-    case 0x86DD:
-      grub_ieee1275_read (handle,datap,sizeof (*ip6h),&actual);
-      ip6h = (struct ip6hdr *) datap;
-
-      datap += sizeof(*ip6h);
-      grub_ieee1275_read (handle,datap,ip6h->payload_len - sizeof (*ip6h),&actual);
-    break;
-
-    default:
-      grub_printf("Unknow packet %x\n",eth->type); 
-    break;
-  }
 //  grub_printf("packsize %d\n",pack->tail - pack->data);
   return 0;// sizeof (eth) + iph.len; 
 }
