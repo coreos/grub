b0ea3a5a93c3ec4eaef2b4bd5fb3126b5b0cae1c
Vladimir 'phcoder' Serbinenko <phcoder@gmail.com>
2010-08-30 16:25:39 +0200

    Add missing noreturn

--- include/grub/misc.h
+++ include/grub/misc.h
@@ -321,9 +321,9 @@ void EXPORT_FUNC (grub_reboot) (void) __attribute__ ((noreturn));
 #ifdef GRUB_MACHINE_PCBIOS
 /* Halt the system, using APM if possible. If NO_APM is true, don't
  * use APM even if it is available.  */
-void grub_halt (int no_apm);
+void grub_halt (int no_apm) __attribute__ ((noreturn));
 #else
-void grub_halt (void);
+void grub_halt (void) __attribute__ ((noreturn));
 #endif
 
 #ifdef GRUB_MACHINE_EMU
