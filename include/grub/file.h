82a85062149a8b22c18d7a803c202a4f6ac9e3bc
Vladimir 'phcoder' Serbinenko <phcoder@gmail.com>
2010-09-05 17:01:16 +0200

    * include/grub/file.h (GRUB_FILE_SIZE_UNKNOWN): New definition. 	* grub-core/disk/loopback.c (grub_loopback_open): Handle unknown file 	size.

--- include/grub/file.h
+++ include/grub/file.h
@@ -104,6 +104,9 @@ grub_ssize_t EXPORT_FUNC(grub_file_read) (grub_file_t file, void *buf,
 grub_off_t EXPORT_FUNC(grub_file_seek) (grub_file_t file, grub_off_t offset);
 grub_err_t EXPORT_FUNC(grub_file_close) (grub_file_t file);
 
+/* Return value of grub_file_size() in case file size is unknown. */
+#define GRUB_FILE_SIZE_UNKNOWN	 0xffffffffffffffffULL
+
 static inline grub_off_t
 grub_file_size (const grub_file_t file)
 {
