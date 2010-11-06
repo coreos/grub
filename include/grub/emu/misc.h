4a1a0153c347f378ee9c43527ca8233bcd21ee1c
Vladimir 'phcoder' Serbinenko <phcoder@gmail.com>
2010-11-06 23:52:56 +0100

    * include/grub/emu/misc.h: Don't include grub/util/libzfs.h. 	* include/grub/emu/misc.h (grub_get_libzfs_handle): Move from here ... 	* include/grub/util/libzfs.h (grub_get_libzfs_handle): ... here.

--- include/grub/emu/misc.h
+++ include/grub/emu/misc.h
@@ -24,7 +24,6 @@
 
 #include <grub/symbol.h>
 #include <grub/types.h>
-#include <grub/util/libzfs.h>
 
 #ifdef __CYGWIN__
 # include <sys/fcntl.h>
@@ -78,7 +77,5 @@ extern char * canonicalize_file_name (const char *path);
 #ifdef HAVE_DEVICE_MAPPER
 int grub_device_mapper_supported (void);
 #endif
-
-libzfs_handle_t *grub_get_libzfs_handle (void);
 
 #endif /* GRUB_EMU_MISC_H */
