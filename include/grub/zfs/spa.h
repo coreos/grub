54207d4b6cd6f8f262e320e453da9462cd046105
Robert Millan <rmh@gnu.org>
2010-12-01 22:55:26 +0100

    Update GPL for ZFS code to version 3, move copyright lines for Vladimir and me to FSF (covered by our assignments)

--- include/grub/zfs/spa.h
+++ include/grub/zfs/spa.h
@@ -1,22 +1,20 @@
 /*
  *  GRUB  --  GRand Unified Bootloader
- *  Copyright (C) 1999,2000,2001,2002,2003,2004  Free Software Foundation, Inc.
+ *  Copyright (C) 1999,2000,2001,2002,2003,2004,2009  Free Software Foundation, Inc.
  *  Copyright 2010 Sun Microsystems, Inc.
- *  Copyright (C) 2009  Vladimir Serbinenko <phcoder@gmail.com>
  *
- *  This program is free software; you can redistribute it and/or modify
+ *  GRUB is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
+ *  GRUB is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef GRUB_ZFS_SPA_HEADER
