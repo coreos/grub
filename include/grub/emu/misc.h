dfdffd0d9467b2b7b397298a0f7420df1e3cdcea
Vladimir 'phcoder' Serbinenko <phcoder@gmail.com>
2012-02-10 14:15:14 +0100

    * include/grub/emu/misc.h (grub_util_warn): Add missing format 	attribute. 	(grub_util_info): Likewise. 	(grub_util_error): Likewise.

--- include/grub/emu/misc.h
+++ include/grub/emu/misc.h
@@ -62,9 +62,9 @@ void * EXPORT_FUNC(xrealloc) (void *ptr, grub_size_t size) __attribute__ ((warn_
 char * EXPORT_FUNC(xstrdup) (const char *str) __attribute__ ((warn_unused_result));
 char * EXPORT_FUNC(xasprintf) (const char *fmt, ...) __attribute__ ((format (printf, 1, 2))) __attribute__ ((warn_unused_result));
 
-void EXPORT_FUNC(grub_util_warn) (const char *fmt, ...);
-void EXPORT_FUNC(grub_util_info) (const char *fmt, ...);
-void EXPORT_FUNC(grub_util_error) (const char *fmt, ...) __attribute__ ((noreturn));
+void EXPORT_FUNC(grub_util_warn) (const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
+void EXPORT_FUNC(grub_util_info) (const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
+void EXPORT_FUNC(grub_util_error) (const char *fmt, ...) __attribute__ ((format (printf, 1, 2), noreturn));
 
 #ifndef HAVE_VASPRINTF
 int EXPORT_FUNC(vasprintf) (char **buf, const char *fmt, va_list ap);
