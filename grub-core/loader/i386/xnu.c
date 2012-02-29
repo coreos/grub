595717e6d4145685bd8790bc64207d7e89e89c35
Vladimir 'phcoder' Serbinenko <phcoder@gmail.com>
2012-02-29 14:53:41 +0100

    * grub-core/loader/i386/xnu.c (grub_xnu_boot): Init fsbfreq to 	sane value to avoid a spurious warning.

--- grub-core/loader/i386/xnu.c
+++ grub-core/loader/i386/xnu.c
@@ -969,7 +969,7 @@ grub_xnu_boot (void)
   grub_size_t devtreelen;
   int i;
   struct grub_relocator32_state state;
-  grub_uint64_t fsbfreq;
+  grub_uint64_t fsbfreq = 100000000;
   int v2 = (grub_xnu_darwin_version >= 11);
   grub_uint32_t efi_system_table = 0;
 
