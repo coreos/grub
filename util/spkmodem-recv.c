600dc5f153e60d643477942d7b261678265a16b6
Vladimir 'phcoder' Serbinenko <phcoder@gmail.com>
2013-01-18 11:54:28 +0100

    Add license header to spkmodem-recv.c.

--- util/spkmodem-recv.c
+++ util/spkmodem-recv.c
@@ -1,3 +1,21 @@
+/* spkmodem-recv.c - decode spkmodem signals */
+/*
+ *  Copyright (C) 2013  Free Software Foundation, Inc.
+ *
+ *  spkmodem-recv is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  spkmodem-recv is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with spkmodem-recv.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
