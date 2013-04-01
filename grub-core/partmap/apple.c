8c3635ff246250363559108dbe45e2ade6643c4e
Radosław Szymczyszyn <lavrin@gmail.com>
2013-04-01 02:58:47 +0200

    * grub-core/partmap/apple.c (apple_partition_map_iterate): Add 	missing closing bracket.

--- grub-core/partmap/apple.c
+++ grub-core/partmap/apple.c
@@ -118,7 +118,7 @@ apple_partition_map_iterate (grub_disk_t disk,
   if (grub_be_to_cpu16 (aheader.magic) != GRUB_APPLE_HEADER_MAGIC)
     {
       grub_dprintf ("partition",
-		    "bad magic (found 0x%x; wanted 0x%x\n",
+		    "bad magic (found 0x%x; wanted 0x%x)\n",
 		    grub_be_to_cpu16 (aheader.magic),
 		    GRUB_APPLE_HEADER_MAGIC);
       goto fail;
@@ -138,7 +138,7 @@ apple_partition_map_iterate (grub_disk_t disk,
       if (grub_be_to_cpu16 (apart.magic) != GRUB_APPLE_PART_MAGIC)
 	{
 	  grub_dprintf ("partition",
-			"partition %d: bad magic (found 0x%x; wanted 0x%x\n",
+			"partition %d: bad magic (found 0x%x; wanted 0x%x)\n",
 			partno, grub_be_to_cpu16 (apart.magic),
 			GRUB_APPLE_PART_MAGIC);
 	  break;
