3d84cb17933852cd8bf90e4bbe8191ce17231df1
Vladimir 'phcoder' Serbinenko <phcoder@gmail.com>
2013-06-14 20:44:59 +0200

    Move flavour-specific parts out of common cpio.c file and 	rename remaining to cpio_common.c

--- grub-core/fs/tar.c
+++ grub-core/fs/tar.c
@@ -1,2 +1,68 @@
+/* cpio.c - cpio and tar filesystem.  */
+/*
+ *  GRUB  --  GRand Unified Bootloader
+ *  Copyright (C) 2007,2008,2009,2013 Free Software Foundation, Inc.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <grub/misc.h>
+
 #define MODE_USTAR 1
-#include "cpio.c"
+
+/* tar support */
+#define MAGIC	"ustar"
+struct head
+{
+  char name[100];
+  char mode[8];
+  char uid[8];
+  char gid[8];
+  char size[12];
+  char mtime[12];
+  char chksum[8];
+  char typeflag;
+  char linkname[100];
+  char magic[6];
+  char version[2];
+  char uname[32];
+  char gname[32];
+  char devmajor[8];
+  char devminor[8];
+  char prefix[155];
+} __attribute__ ((packed));
+
+static inline unsigned long long
+read_number (const char *str, grub_size_t size)
+{
+  unsigned long long ret = 0;
+  while (size-- && *str >= '0' && *str <= '7')
+    ret = (ret << 3) | (*str++ & 0xf);
+  return ret;
+}
+
+#define FSNAME "tarfs"
+
+#include "cpio_common.c"
+
+GRUB_MOD_INIT (tar)
+{
+  grub_fs_register (&grub_cpio_fs);
+  my_mod = mod;
+}
+
+GRUB_MOD_FINI (tar)
+{
+  grub_fs_unregister (&grub_cpio_fs);
+}
--- include/grub/fat.h
+++ include/grub/fat.h
@@ -29,13 +29,18 @@ struct grub_fat_bpb
   grub_uint8_t sectors_per_cluster;
   grub_uint16_t num_reserved_sectors;
   grub_uint8_t num_fats;
+  /* 0x10 */
   grub_uint16_t num_root_entries;
   grub_uint16_t num_total_sectors_16;
   grub_uint8_t media;
+  /*0 x15 */
   grub_uint16_t sectors_per_fat_16;
   grub_uint16_t sectors_per_track;
+  /*0 x19 */
   grub_uint16_t num_heads;
+  /*0 x1b */
   grub_uint32_t num_hidden_sectors;
+  /* 0x1f */
   grub_uint32_t num_total_sectors_32;
   union
   {
