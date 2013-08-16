d5c1eef106f40881b0f883fdf6e9caf04b144cc8
Vladimir 'phcoder' Serbinenko <phcoder@gmail.com>
2013-08-16 14:10:37 +0200

    * grub-core/lib/posix_wrap/wchar.h: Fix wchar_t and mbstate_t conflict 	on NetBSD and OpenBSD.

--- grub-core/lib/posix_wrap/wchar.h
+++ grub-core/lib/posix_wrap/wchar.h
@@ -21,6 +21,11 @@
 
 #include <grub/charset.h>
 
+#if defined(__NetBSD__) || defined(__OPENBSD__)
+#define wchar_t grub_posix_wchar_t
+#define mbstate_t grub_posix_mbstate_t
+#endif
+
 /* UCS-4.  */
 typedef grub_int32_t wint_t;
 enum
