5177ba140070771acf0a28b5223db580c9847674
Vladimir 'phcoder' Serbinenko <phcoder@gmail.com>
2013-09-24 18:55:10 +0200

    * util/getroot.c (grub_util_check_block_device): Move to ... 	* util/getroot_unix.c (grub_util_check_block_device): ... here. 	* util/getroot.c (grub_util_check_char_device): Move to ... 	* util/getroot_unix.c (grub_util_check_char_device): ... here.

--- util/getroot.c
+++ util/getroot.c
@@ -473,35 +473,6 @@ grub_util_biosdisk_is_present (const char *os_dev)
   return ret;
 }
 
-const char *
-grub_util_check_block_device (const char *blk_dev)
-{
-  struct stat st;
-
-  if (stat (blk_dev, &st) < 0)
-    grub_util_error (_("cannot stat `%s': %s"), blk_dev,
-		     strerror (errno));
-
-  if (S_ISBLK (st.st_mode))
-    return (blk_dev);
-  else
-    return 0;
-}
-
-const char *
-grub_util_check_char_device (const char *blk_dev)
-{
-  struct stat st;
-
-  if (stat (blk_dev, &st) < 0)
-    grub_util_error (_("cannot stat `%s': %s"), blk_dev, strerror (errno));
-
-  if (S_ISCHR (st.st_mode))
-    return (blk_dev);
-  else
-    return 0;
-}
-
 #ifdef HAVE_LIBZFS
 static libzfs_handle_t *__libzfs_handle;
 
