a1a315e780d7cbb0e4f40b31e74b5c87278a65e1
Vladimir 'phcoder' Serbinenko <phcoder@gmail.com>
2013-10-08 17:01:04 +0200

    * include/grub/setjmp.h: Remove leftover GRUBOF.

--- include/grub/setjmp.h
+++ include/grub/setjmp.h
@@ -19,7 +19,7 @@
 #ifndef GRUB_SETJMP_HEADER
 #define GRUB_SETJMP_HEADER	1
 
-#if defined(GRUB_UTIL) && !defined(GRUBOF)
+#if defined(GRUB_UTIL)
 #include <setjmp.h>
 typedef jmp_buf grub_jmp_buf;
 #define grub_setjmp setjmp
