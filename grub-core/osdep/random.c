d0a116ee5a6cc14df759df175c8f02883b9d955c
Vladimir 'phcoder' Serbinenko <phcoder@gmail.com>
2013-10-19 02:30:05 +0200

    * grub-core/osdep/random.c: Use unix/random.c on haiku. Haiku uses 	yarrow (by B. Schneier et al) for its /dev/urandom (similar to FreeBSD).

--- grub-core/osdep/random.c
+++ grub-core/osdep/random.c
@@ -3,7 +3,7 @@
 #elif defined (__linux__) || defined (__FreeBSD__) \
   || defined (__FreeBSD_kernel__) || defined (__OpenBSD__) \
   || defined (__GNU__) || defined (__NetBSD__) \
-  || defined (__APPLE__) || defined(__sun__)
+  || defined (__APPLE__) || defined(__sun__) || defined (__HAIKU__)
 #include "unix/random.c"
 #else
 #include "basic/random.c"
