b61599aca4b4227bb40f69ab02a08738f6457d01
Vladimir Serbinenko <phcoder@gmail.com>
2013-11-01 14:39:33 +0100

    * grub-core/kern/mm.c (grub_realloc): Don't copy more data than we have.

--- grub-core/kern/mm.c
+++ grub-core/kern/mm.c
@@ -483,7 +483,8 @@ grub_realloc (void *ptr, grub_size_t size)
   if (! q)
     return q;
 
-  grub_memcpy (q, ptr, size);
+  /* We've already checked that p->size < n.  */
+  grub_memcpy (q, ptr, p->size << GRUB_MM_ALIGN_LOG2);
   grub_free (ptr);
   return q;
 }
