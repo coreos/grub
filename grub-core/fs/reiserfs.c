70bb6c855e46d7fb807456606904b6a78817e6be
Vladimir Serbinenko <phcoder@gmail.com>
2013-11-07 00:48:04 +0100

    * grub-core/fs/reiserfs.c (grub_reiserfs_iterate_dir): Fix type of 	entry_type.

--- grub-core/fs/reiserfs.c
+++ grub-core/fs/reiserfs.c
@@ -781,7 +781,7 @@ grub_reiserfs_iterate_dir (grub_fshelp_node_t item,
             = grub_le_to_cpu16 (directory_header->state);
 	  grub_fshelp_node_t entry_item;
 	  struct grub_reiserfs_key entry_key;
-	  enum grub_reiserfs_item_type entry_type;
+	  enum grub_fshelp_filetype entry_type;
 	  char *entry_name;
 	  
           if (!(entry_state & GRUB_REISERFS_VISIBLE_MASK))
