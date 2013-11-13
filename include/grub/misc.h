5e77d9cfc1e22ecaaa99af0d5ae0fc05365402ce
Vladimir Serbinenko <phcoder@gmail.com>
2013-11-13 09:52:33 +0100

    * grub-core/kern/arm/misc.S: Add __muldi3 and __aeabi_lmul. Those 	helper functions are needed for thumb.

--- include/grub/misc.h
+++ include/grub/misc.h
@@ -469,6 +469,12 @@ EXPORT_FUNC (__aeabi_uidiv) (grub_uint32_t a, grub_uint32_t b);
 grub_uint32_t
 EXPORT_FUNC (__aeabi_uidivmod) (grub_uint32_t a, grub_uint32_t b);
 
+/* Needed for allowing modules to be compiled as thumb.  */
+grub_uint64_t
+EXPORT_FUNC (__muldi3) (grub_uint64_t a, grub_uint64_t b);
+grub_uint64_t
+EXPORT_FUNC (__aeabi_lmul) (grub_uint64_t a, grub_uint64_t b);
+
 #endif
 
 #if defined (__ia64__)
