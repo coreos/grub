740c08ed2ff33625bdc2e0ad9ef6264978bf32cf
Andrei Borzenkov <arvidjaar@gmail.com>
2014-11-28 21:43:07 +0300

    Use ssize_t for grub_util_fd_read result
    Fixes commit 0415dbe28f3dc3520b70745b3bd78073f094fb4d.

--- util/grub-install-common.c
+++ util/grub-install-common.c
@@ -902,7 +902,7 @@ grub_install_get_target (const char *src)
   char *fn;
   grub_util_fd_t f;
   char buf[2048];
-  size_t r;
+  ssize_t r;
   char *c, *pl, *p;
   size_t i;
   fn = grub_util_path_concat (2, src, "modinfo.sh");
