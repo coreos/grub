68581b009f6e943bd6ddf3c781db1c884832fa8e
Vladimir Serbinenko <phcoder@gmail.com>
2015-01-21 10:28:52 +0100

    unix/cputime.c: Cache sc_clk_tck and check it for sanity.

--- grub-core/osdep/unix/cputime.c
+++ grub-core/osdep/unix/cputime.c
@@ -9,7 +9,14 @@ grub_uint64_t
 grub_util_get_cpu_time_ms (void)
 {
   struct tms tm;
+  static long sc_clk_tck;
+  if (!sc_clk_tck)
+    {
+      sc_clk_tck = sysconf(_SC_CLK_TCK);
+      if (sc_clk_tck <= 0)
+	sc_clk_tck = 1000;
+    }
 
   times (&tm); 
-  return (tm.tms_utime * 1000ULL) / sysconf(_SC_CLK_TCK);
+  return (tm.tms_utime * 1000ULL) / sc_clk_tck;
 }
