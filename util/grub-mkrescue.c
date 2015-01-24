eeea31b2208d862a3ff50f2739e103b22dc0061b
Vladimir Serbinenko <phcoder@gmail.com>
2015-01-24 17:45:16 +0100

    util/grub-mkrescue.c: Always include part_msdos and part_gpt on EFI.
    When booted from stick, EFI would use GPT partition and our root
    device detection algortihm depends on GRUB's ability to see the same
    partitions. Hence include msdos and gpt partmap modules on EFI even when
    they're not needed to access root filesystem.

--- util/grub-mkrescue.c
+++ util/grub-mkrescue.c
@@ -653,6 +653,9 @@ main (int argc, char *argv[])
       char *efiimgfat;
       grub_install_mkdir_p (efidir_efi_boot);
 
+      grub_install_push_module ("part_gpt");
+      grub_install_push_module ("part_msdos");
+
       imgname = grub_util_path_concat (2, efidir_efi_boot, "bootia64.efi");
       make_image_fwdisk_abs (GRUB_INSTALL_PLATFORM_IA64_EFI, "ia64-efi", imgname);
       free (imgname);
@@ -713,6 +716,8 @@ main (int argc, char *argv[])
       free (efiimgfat);
       free (efidir_efi);
       free (efidir);
+      grub_install_pop_module ();
+      grub_install_pop_module ();
     }
 
   grub_install_push_module ("part_apple");
