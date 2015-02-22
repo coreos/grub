50ebc8f5f35a322b3736c03e16de7108932126b0
Vladimir Serbinenko <phcoder@gmail.com>
2015-02-22 13:12:44 +0100

    i386/tsc: Fix unused function warning on xen.

--- grub-core/kern/i386/tsc.c
+++ grub-core/kern/i386/tsc.c
@@ -57,6 +57,8 @@ grub_get_tsc (void)
   return (((grub_uint64_t) hi) << 32) | lo;
 }
 
+#ifndef GRUB_MACHINE_XEN
+
 static __inline int
 grub_cpu_is_tsc_supported (void)
 {
@@ -68,8 +70,6 @@ grub_cpu_is_tsc_supported (void)
 
   return (d & (1 << 4)) != 0;
 }
-
-#ifndef GRUB_MACHINE_XEN
 
 static void
 grub_pit_wait (grub_uint16_t tics)
