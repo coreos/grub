8d3c4544ffdd0289a4b0bdeb0cdc6355f801a4b3
Andrei Borzenkov <arvidjaar@gmail.com>
2015-05-04 08:08:57 +0300

    efinet: memory leak on module removal

--- grub-core/net/drivers/efi/efinet.c
+++ grub-core/net/drivers/efi/efinet.c
@@ -278,6 +278,12 @@ GRUB_MOD_FINI(efinet)
 
   FOR_NET_CARDS_SAFE (card, next) 
     if (card->driver == &efidriver)
-      grub_net_card_unregister (card);
+      {
+	grub_net_card_unregister (card);
+	grub_free (card->txbuf);
+	grub_free (card->rcvbuf);
+	grub_free (card->name);
+	grub_free (card);
+      }
 }
 
