cfabe05cc6ab88fc7c7a48c9bc0072770b7ad101
Vladimir Serbinenko <phcoder@gmail.com>
2015-10-11 13:15:50 +0200

    mips: Make setjmp code N32-compliant.

--- include/grub/mips/asm.h
+++ include/grub/mips/asm.h
@@ -4,8 +4,15 @@
 #if defined(_MIPS_SIM) && defined(_ABIN32) && _MIPS_SIM == _ABIN32
 #define GRUB_ASM_T4 $a4
 #define GRUB_ASM_T5 $a5
+#define GRUB_ASM_SZREG 8
+#define GRUB_ASM_REG_S sd
+#define GRUB_ASM_REG_L ld
 #else
 #define GRUB_ASM_T4 $t4
 #define GRUB_ASM_T5 $t5
+#define GRUB_ASM_SZREG 4
+#define GRUB_ASM_REG_S sw
+#define GRUB_ASM_REG_L lw
 #endif
+
 #endif
