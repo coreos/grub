7c6c2ad42c3a3a2cbe73ef7808cc6b85fc895ac0
Vladimir Serbinenko <phcoder@gmail.com>
2016-01-08 16:22:19 +0100

    i386-ieee1275: Increase maximum heap size to accomodate highres graphi tests

--- grub-core/kern/ieee1275/init.c
+++ grub-core/kern/ieee1275/init.c
@@ -46,11 +46,19 @@
 #define HEAP_MIN_SIZE		(unsigned long) (2 * 1024 * 1024)
 
 /* The maximum heap size we're going to claim */
+#ifdef __i386__
+#define HEAP_MAX_SIZE		(unsigned long) (64 * 1024 * 1024)
+#else
 #define HEAP_MAX_SIZE		(unsigned long) (32 * 1024 * 1024)
+#endif
 
 /* If possible, we will avoid claiming heap above this address, because it
    seems to cause relocation problems with OSes that link at 4 MiB */
+#ifdef __i386__
+#define HEAP_MAX_ADDR		(unsigned long) (64 * 1024 * 1024)
+#else
 #define HEAP_MAX_ADDR		(unsigned long) (32 * 1024 * 1024)
+#endif
 
 extern char _start[];
 extern char _end[];
