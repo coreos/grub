dab6a0f0c31d2805fff1f58e5c37e21260602b6b
Michael Marineau <michael.marineau@coreos.com>
2016-10-19 14:12:55 -0700

    loader: validate cmdline string length before appending verity arg

--- grub-core/loader/i386/verity-hash.h
+++ grub-core/loader/i386/verity-hash.h
@@ -1,9 +1,11 @@
 #define VERITY_ARG " verity.usrhash="
+#define VERITY_ARG_LENGTH (sizeof (VERITY_ARG) - 1)
 #define VERITY_HASH_OFFSET 0x40
 #define VERITY_HASH_LENGTH 64
 
 static inline void grub_pass_verity_hash(struct linux_kernel_header *lh,
-					 char *cmdline)
+					 char *cmdline,
+					 grub_size_t cmdline_max_len)
 {
   char *buf = (char *)lh;
   grub_size_t cmdline_len;
@@ -16,10 +18,14 @@ static inline void grub_pass_verity_hash(struct linux_kernel_header *lh,
 	  return;
     }
 
-  grub_memcpy (cmdline + grub_strlen(cmdline), VERITY_ARG,
-	       sizeof (VERITY_ARG));
   cmdline_len = grub_strlen(cmdline);
+  if (cmdline_len + VERITY_ARG_LENGTH + VERITY_HASH_LENGTH > cmdline_max_len)
+    return;
+
+  grub_memcpy (cmdline + cmdline_len, VERITY_ARG, VERITY_ARG_LENGTH);
+  cmdline_len += VERITY_ARG_LENGTH;
   grub_memcpy (cmdline + cmdline_len, buf + VERITY_HASH_OFFSET,
 	       VERITY_HASH_LENGTH);
-  cmdline[cmdline_len + VERITY_HASH_LENGTH] = '\0';
+  cmdline_len += VERITY_HASH_LENGTH;
+  cmdline[cmdline_len] = '\0';
 }
