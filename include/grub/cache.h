d8901e3ba115d705c9c08ecedd201aca8b4867b8
phcoder <phcoder@sid.debian.laptop.phnet>
2017-07-09 20:59:15 +0200

    cache: Fix compilation for ppc, sparc and arm64

--- include/grub/cache.h
+++ include/grub/cache.h
@@ -34,7 +34,9 @@ void EXPORT_FUNC(grub_arch_sync_caches) (void *address, grub_size_t len);
 #endif
 
 #ifndef GRUB_MACHINE_EMU
-#if defined (__i386__) || defined (__x86_64__)
+#if defined (__aarch64__) || defined (__powerpc__) || defined (__sparc__)
+
+#elif defined (__i386__) || defined (__x86_64__)
 static inline void
 grub_arch_sync_dma_caches (volatile void *address __attribute__ ((unused)),
 			   grub_size_t len __attribute__ ((unused)))
