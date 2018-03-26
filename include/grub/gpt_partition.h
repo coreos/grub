563b1da6e6ae7af46cc8354cadb5dab416989f0a
Michael Chang <mchang@suse.com>
2018-03-26 16:52:34 +0800

    Fix packed-not-aligned error on GCC 8
    When building with GCC 8, there are several errors regarding packed-not-aligned.
    
    ./include/grub/gpt_partition.h:79:1: error: alignment 1 of ‘struct grub_gpt_partentry’ is less than 8 [-Werror=packed-not-aligned]
    
    This patch fixes the build error by cleaning up the ambiguity of placing
    aligned structure in a packed one. In "struct grub_btrfs_time" and "struct
    grub_gpt_part_type", the aligned attribute seems to be superfluous, and also
    has to be packed, to ensure the structure is bit-to-bit mapped to the format
    laid on disk. I think we could blame to copy and paste error here for the
    mistake. In "struct efi_variable", we have to use grub_efi_packed_guid_t, as
    the name suggests. :)
    
    Signed-off-by: Michael Chang <mchang@suse.com>
    Tested-by: Michael Chang <mchang@suse.com>
    Tested-by: Paul Menzel <paulepanter@users.sourceforge.net>
    Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>

--- include/grub/gpt_partition.h
+++ include/grub/gpt_partition.h
@@ -28,7 +28,7 @@ struct grub_gpt_part_type
   grub_uint16_t data2;
   grub_uint16_t data3;
   grub_uint8_t data4[8];
-} __attribute__ ((aligned(8)));
+} GRUB_PACKED;
 typedef struct grub_gpt_part_type grub_gpt_part_type_t;
 
 #define GRUB_GPT_PARTITION_TYPE_EMPTY \
